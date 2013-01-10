f008d912ee0f318ad47ff18e9af11a43014f4230
Jamie English <jamienglish@gmail.com>
2013-01-10 21:25:57 +0000

    init rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goldfinchjewellery::Application.config.secret_key_base = 'fdbd3dd6ceda20b4a24ac3686ca87feca466ad3442104ac325f7986aeb9007dcd0bf2af2d9ca4a0ae5c2f500b251c5890170f24edd0154e420a3ac44c8d5a2cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goldfinchjewellery::Application.config.session_store :encrypted_cookie_store, key: '_goldfinchjewellery_session'
