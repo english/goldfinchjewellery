2c1d34be8678defb8dc8b44bb1002a5e861e98bd
Jamie English <jamienglish@gmail.com>
2013-01-12 13:15:53 +0000

    Refactored navigation into a helper method

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  def navigation links
+    links.map { |link_text, url|
+      content_tag :li, list_item_options_for(link_text) do
+        link_to link_text, url
+      end
+    }.join
+  end
+
+  def list_item_options_for text
+    { class: 'current' } if current_page? controller: 'pages', action: text.parameterize.underscore
+  end
 end
