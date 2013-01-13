628d1d9696049b9562f41830b725d294ea3c3e55
Jamie English <jamienglish@gmail.com>
2013-01-13 11:44:06 +0000

    Moved navigation helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def navigation links
+    links.map { |link_text, path|
+      content_tag :li, list_item_options_for(path) do
+        link_to link_text, path
+      end
+    }.join "\n"
+  end
+
+  def list_item_options_for path
+    { class: 'current' } if request.fullpath == path
+  end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,2 @@
 module PagesHelper
-  def navigation links
-    links.map { |link_text, path|
-      content_tag :li, list_item_options_for(link_text) do
-        link_to link_text, path
-      end
-    }.join "\n"
-  end
-
-  def list_item_options_for text
-    { class: 'current' } if current_page? controller: 'pages', action: text.parameterize.underscore
-  end
 end
