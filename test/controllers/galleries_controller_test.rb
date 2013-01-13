ce81caf9a8bd7e6bcadf608a8d5fddd7208117cb
Jamie English <jamienglish@gmail.com>
2013-01-13 22:30:48 +0000

    DRYed up gallery tests

--- test/controllers/galleries_controller_test.rb
+++ test/controllers/galleries_controller_test.rb
@@ -1,45 +1,27 @@
 require 'test_helper'
 
 class GalleriesControllerTest < ActionController::TestCase
+  PAGES = %w[weather peace-doves birds commissions woodlands]
+
   test "index" do
     get :index
-    assert_select 'nav li.current a', 'Gallery'
     assert_tag tag: 'img', attributes: { src: /gallery.jpg/ }
   end
 
-  test "GET /gallery/peace-doves" do
-    get :show, id: 'peace-doves'
-    assert_template partial: 'galleries/_peace_doves'
+  test "gallery is selected in nav" do
+    get :index
     assert_select 'nav li.current a', 'Gallery'
+
+    PAGES.each do |page|
+      get :show, id: page
+      assert_select 'nav li.current a', 'Gallery'
+    end
   end
 
-  test "GET /gallery/weather" do
-    get :show, id: 'weather'
-    assert_template partial: 'galleries/_weather'
-    assert_select 'nav li.current a', 'Gallery'
-  end
-
-  test "GET /gallery/birds" do
-    get :show, id: 'birds'
-    assert_template partial: 'galleries/_birds'
-    assert_select 'nav li.current a', 'Gallery'
-  end
-
-  test "GET /gallery/commissions" do
-    get :show, id: 'commissions'
-    assert_template partial: 'galleries/_commissions'
-    assert_select 'nav li.current a', 'Gallery'
-  end
-
-  test "GET /gallery/branches" do
-    get :show, id: 'branches'
-    assert_template partial: 'galleries/_branches'
-    assert_select 'nav li.current a', 'Gallery'
-  end
-
-  test "GET /gallery/woodlands" do
-    get :show, id: 'woodlands'
-    assert_template partial: 'galleries/_woodlands'
-    assert_select 'nav li.current a', 'Gallery'
+  test "GET /gallery/:id renders correct partial" do
+    PAGES.each do |page|
+      get :show, id: page
+      assert_template partial: "galleries/_#{page.underscore}"
+    end
   end
 end
