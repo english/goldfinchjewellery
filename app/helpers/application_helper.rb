296e9fd6d193e84352644f7b94a197ecbc2f1aee
Jamie English <jamienglish@gmail.com>
2013-01-13 22:30:30 +0000

    Catered for root page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,18 @@
+require 'pry'
+
 module ApplicationHelper
   def navigation links
     links.map { |link_text, path|
-      content_tag :li, list_item_options_for(path) do
+      content_tag :li, list_item_options_for(link_text, path) do
         link_to link_text, path
       end
     }.join "\n"
   end
 
-  def list_item_options_for path
+  def list_item_options_for link_text, path
     first_part_of_path       = request.fullpath.split('/').second
     path_minus_leading_slash = path[1..-1]
 
-    { class: 'current' } if first_part_of_path == path_minus_leading_slash
+    { class: 'current' } if first_part_of_path == path_minus_leading_slash || (link_text == 'About' && request.fullpath == '/')
   end
 end
