505978eac949d69465c202c6dd441b8f3cc8f57b
Jamie English <jamienglish@gmail.com>
2013-01-19 21:00:45 +0000

    Turned on caching in development to identify errors earlier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_expires_in
+
+  private
+
+  def set_expires_in
+    expires_in 10.minutes, public: true
+  end
 end
