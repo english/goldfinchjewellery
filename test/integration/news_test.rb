6e362c3a7d0e01dc33447b54a41ffe74fc612fff
Jamie English <jamienglish@gmail.com>
2013-01-24 19:16:27 +0000

    Added flash message to successful news_item save

--- test/integration/news_test.rb
+++ test/integration/news_test.rb
@@ -12,6 +12,8 @@ class NewsTest < ActionDispatch::IntegrationTest
     fill_in 'Content', with: 'Test news story'
     click_button 'Save and Publish'
 
+    assert page.has_selector?('.success', text: 'News Item saved successfully')
+
     visit '/'
     click_link 'Latest News'
     assert page.has_text? 'Test news story'
