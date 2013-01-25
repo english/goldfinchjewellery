00ebcb7be81ebcfbea59e2a69e613480fd78d002
Jamie English <jamienglish@gmail.com>
2013-01-25 20:47:41 +0000

    Some code I'd like to have

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -5,8 +5,10 @@ class NewsController < ApplicationController
   end
 
   def create
-    attributes = params.require(:news_item).permit [:category, :content, :image_path]
-    @news_item = NewsItem.new attributes
+    s3image = S3Image.new image_param
+    s3image.store!
+
+    @news_item = NewsItem.new(news_item_params.merge(image_path: s3image.url))
 
     if @news_item.save
       redirect_to news_index_path, notice: 'News Item saved successfully'
@@ -18,5 +20,15 @@ class NewsController < ApplicationController
 
   def index
     @categorised_news_items = NewsItem.categorised
+  end
+
+  private
+  
+  def news_item_params
+    params.require(:news_item).permit [:category, :content]
+  end
+
+  def image_param
+    params.require(:news_item).permit :image_path
   end
 end
