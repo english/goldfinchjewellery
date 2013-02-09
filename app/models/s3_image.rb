0922464f44994c1d5cdf708342879f84c0b479ed
Jamie English <jamienglish@gmail.com>
2013-02-09 22:16:12 +0000

    More tidy up

--- app/models/s3_image.rb
+++ app/models/s3_image.rb
@@ -1,20 +1,27 @@
 class S3Image
-  def initialize file
+  def initialize(file)
     @file = file
   end
 
   def store!
     http = Net::HTTP.new("goldfinchjewellery.s3-eu-west-1.amazonaws.com")
-    request = Net::HTTP::Put.new('/' + @file.original_filename)
-
-    headers = S3Headers.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], @file)
-    request.initialize_http_header(headers.to_hash)
-
-    request.body_stream = File.open(@file.path)
-    response = http.request(request)
+    http.request(put_request)
   end
 
   def url
     "http://goldfinchjewellery.s3-eu-west-1.amazonaws.com/#{@file.original_filename}"
+  end
+
+  private
+
+  def put_request
+    path = '/' + @file.original_filename
+    request = Net::HTTP::Put.new(path)
+    headers = S3Headers.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], @file)
+
+    request.initialize_http_header(headers.to_hash)
+    request.body_stream = File.open(@file.path)
+
+    request
   end
 end
