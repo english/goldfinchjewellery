f1390117645acbf8c34054ecb49cec9b6a37d2ad
Jamie English <jamienglish@gmail.com>
2013-02-23 19:38:45 +0000

    Extracted sign_in helper

--- test/integration/authentication_test.rb
+++ test/integration/authentication_test.rb
@@ -6,38 +6,33 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
   test "Admin signs in and can edit stuff" do
     visit '/'
     click_link 'Latest News'
-
     refute page.has_link?('Delete'), 'Page should not have any Delete links'
     refute page.has_link?('New News Item'), 'Page should not have a New News Item link'
 
-    visit '/sign_in'
-
-    fill_in 'Email', with: 'someone@example.org'
-    fill_in 'Password', with: 'secret'
-    click_button 'Sign in'
-
+    sign_in
     assert page.has_selector?('.success', text: 'Signed in successfully')
 
     visit '/'
     click_link 'Latest News'
-
     assert page.has_link?('Delete')
     assert page.has_link?('New News Item')
   end
 
   test "Admin can sign in and then sign out" do
-    visit '/sign_in'
+    sign_in
 
-    fill_in 'Email', with: 'someone@example.org'
-    fill_in 'Password', with: 'secret'
-    click_button 'Sign in'
     click_link 'Latest News'
-
     assert page.has_link?('Delete')
 
     click_link 'Sign out'
     click_link 'Latest News'
-
     refute page.has_link?('Delete')
+  end
+
+  def sign_in
+    visit '/sign_in'
+    fill_in 'Email', with: 'someone@example.org'
+    fill_in 'Password', with: 'secret'
+    click_button 'Sign in'
   end
 end
