4fb40fb2e53a43da64f6a6fc6d5f66a02fb2395b
Jamie English <jamienglish@gmail.com>
2013-03-28 21:37:22 +0000

    Integration test for creating jewellery piece

---
+++ test/integration/galleries_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class GalleriesTest < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  test "creating a gallery entry" do
+    sign_in
+    visit '/admin'
+    click_link 'New Piece of Jewellery'
+
+    fill_in 'Name', with: 'Test Jewellery'
+    fill_in 'Description', with: 'Test piece of jewellery description'
+    select 'Peace Doves', from: 'Gallery'
+    attach_file 'Image', 'test/fixtures/image.jpg'
+    click_button 'Save and Publish'
+
+    visit '/'
+    click_link 'Gallery'
+    click_link 'Peace Doves'
+    assert page.has_content? 'Test piece of jewellery description'
+    assert page.has_selector?('img', alt: 'Test Jewellery')
+  end
+end
