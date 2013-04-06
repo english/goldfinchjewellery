73f893c44bca8eef48ce7f642786a02df1fd29f0
Jamie English <jamienglish@gmail.com>
2013-04-06 18:35:59 +0100

    encrypted cookie store was removed in rails 4

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Goldfinchjewellery::Application.config.session_store :encrypted_cookie_store, key: '_goldfinchjewellery_session'
+Goldfinchjewellery::Application.config.session_store :cookie_store, key: '_goldfinchjewellery_session'
