4ea721a7d9572e25b9ce19cf0b728da0bbff0abe
Jamie English <jamienglish@gmail.com>
2014-04-11 21:34:15 +0100

    Follow Rails 4.1 conventions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,8 +10,6 @@ VCR.configure do |config|
 end
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
